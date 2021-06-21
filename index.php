936c17daed08ceae5cc316a1e480cb602b6eaa6b
Li8ning <jadejadharmraj5@gmail.com>
2021-06-21 15:31:41 +0530

    Hero Section for 1366px and 1920px done

--- index.php
+++ index.php
@@ -4,9 +4,9 @@ include('header.php');?>
 
 <div class="container-fluid container-xxl px-xl-4 px-xxl-0">
     <!-- Primary Links -->
-    <section class="primary-links av-font-iv mt-5 mb-4 text-center">
+    <section class="primary-links av-font-v mt-5 text-center">
         <div class="dropdown btn-group">
-            <button class="btn dropdown-toggle av-font-color-ii av-font-iv fw-bold" type="button" id="p-link-btn1" data-bs-toggle="dropdown" aria-expanded="false">
+            <button class="btn dropdown-toggle av-font-color-ii av-font-v fw-bold" type="button" id="p-link-btn1" data-bs-toggle="dropdown" aria-expanded="false">
             <i class="fas fa-user me-3"></i>Personal<i class="fas fa-chevron-down ms-3 btn-arrow"></i>
             </button>
             <div class="dropdown-menu p-4" aria-labelledby="p-link-btn1">
@@ -42,7 +42,7 @@ include('header.php');?>
             </div>
         </div>
         <div class="dropdown btn-group">
-            <button class="btn dropdown-toggle av-font-color-ii av-font-iv fw-bold" type="button" id="p-link-btn1" data-bs-toggle="dropdown" aria-expanded="false">
+            <button class="btn dropdown-toggle av-font-color-ii av-font-v fw-bold" type="button" id="p-link-btn1" data-bs-toggle="dropdown" aria-expanded="false">
             <i class="fas fa-seedling me-3"></i>PACS<i class="fas fa-chevron-down ms-3 btn-arrow"></i>
             </button>
             <div class="dropdown-menu p-4" aria-labelledby="p-link-btn1">
@@ -78,7 +78,7 @@ include('header.php');?>
             </div>
         </div>
         <div class="dropdown btn-group">
-            <button class="btn dropdown-toggle av-font-color-ii av-font-iv fw-bold" type="button" id="p-link-btn1" data-bs-toggle="dropdown" aria-expanded="false">
+            <button class="btn dropdown-toggle av-font-color-ii av-font-v fw-bold" type="button" id="p-link-btn1" data-bs-toggle="dropdown" aria-expanded="false">
             <i class="fas fa-briefcase me-3"></i>Business<i class="fas fa-chevron-down ms-3 btn-arrow"></i>
             </button>
             <div class="dropdown-menu p-4" aria-labelledby="p-link-btn1">
@@ -115,6 +115,56 @@ include('header.php');?>
         </div>
     </section>
     <!-- Primary Links End-->
+
+    <!-- Hero Section -->
+    <section class="hero-section mt-4">
+        <div class="row align-items-center">
+            <div class="col-4 left-col">
+                <h1 class="av-font-viii av-font-color-ii">Kerala's Own Bank</h1>
+                <p class="av-font-ii">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Convallis neque mauris feugiat pulvinar egestas.</p>
+                <a class="btn av-font-ii av-font-color-iii">Explore Top Schemes</a>
+            </div>
+            <div class="col-8 right-col">
+                <div id="kb-hero-slider" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-indicators">
+                        <button type="button" data-bs-target="#kb-hero-slider" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#kb-hero-slider" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#kb-hero-slider" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    </div>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <a href="">
+                                <img src="./assets/img/slider-1.jpg" class="d-block w-100" alt="">
+                            </a>                            
+                        </div>
+                        <div class="carousel-item">
+                            <a href="">
+                                <img src="./assets/img/slider-1.jpg" class="d-block w-100" alt="">
+                            </a>
+                        </div>
+                        <div class="carousel-item">
+                            <a href="">
+                                <img src="./assets/img/slider-1.jpg" class="d-block w-100" alt="">
+                            </a>
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev slider-controls" type="button" data-bs-target="#kb-hero-slider" data-bs-slide="prev">
+                        <i class="fas fa-arrow-circle-left carousel-control-prev-icon" aria-hidden="true"></i>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next slider-controls" type="button" data-bs-target="#kb-hero-slider" data-bs-slide="next">
+                    <i class="fas fa-arrow-circle-right carousel-control-prev-icon" aria-hidden="true"></i>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Hero Section End-->
+
+    <!-- Serives/Third Section -->
+    <section class="services-section mt-4"></section>
+    <!-- Serives/Third Section Ends-->
 </div>
 
 <?php

7bc3dfebba458f2aa23f0238378bdf15323d6db8
Li8ning <jadejadharmraj5@gmail.com>
2021-06-21 12:36:45 +0530

    Primary Link Button Complete for 1366px and 1920px

--- assets/js/style.js
+++ assets/js/style.js
@@ -9,4 +9,23 @@ $(document).ready(function(){
             $(this).find(".dropdown-toggle").blur();
         }
     });
+
+    // Primary Links Dropdown
+    var primary_link_btn = $('.primary-links button');
+    primary_link_btn.on('show.bs.dropdown',function(){
+        $(this).css({
+            'background': '#C24848',
+            'color': '#FFDAA5',
+            'box-shadow': '-4px 5px 14px -6px rgba(0, 0, 0, 0.3)'
+        });
+        $(this).find('.btn-arrow').css('transform','rotate(-90deg)');
+    });
+    primary_link_btn.on('hide.bs.dropdown',function(){
+        $(this).css({
+            'background': '#FFFFFF',
+            'color': '#C24848',
+            'box-shadow': '-4px 5px 14px -6px rgba(0, 0, 0, 0.3)'
+        });
+        $(this).find('.btn-arrow').css('transform','rotate(0deg)');
+    });
 });
--- index.php
+++ index.php
@@ -4,22 +4,114 @@ include('header.php');?>
 
 <div class="container-fluid container-xxl px-xl-4 px-xxl-0">
     <!-- Primary Links -->
-    <section class="primary-links">
-        <div class="row">
-            <div class="col-4">
-                <div class="dropdown">
-                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                        Personal
-                    </button>
-                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
+    <section class="primary-links av-font-iv mt-5 mb-4 text-center">
+        <div class="dropdown btn-group">
+            <button class="btn dropdown-toggle av-font-color-ii av-font-iv fw-bold" type="button" id="p-link-btn1" data-bs-toggle="dropdown" aria-expanded="false">
+            <i class="fas fa-user me-3"></i>Personal<i class="fas fa-chevron-down ms-3 btn-arrow"></i>
+            </button>
+            <div class="dropdown-menu p-4" aria-labelledby="p-link-btn1">
+                <div class="row">
+                    <div class="col d-flex justify-content-center">
+                        <ul class="list-unstyled">
+                            <li><h6 class="dropdown-header av-font-v fw-bold">Accounts & Deposits</h6></li>
+                            <li><a class="dropdown-item av-font-ii" href="#">Savings</a></li>
+                            <li><a class="dropdown-item av-font-ii" href="#">Current</a></li>
+                            <li><a class="dropdown-item av-font-ii" href="#">Recurring Deposit</a></li>
+                            <li><a class="dropdown-item av-font-ii" href="#">Term Deposit</a></li>
+                        </ul>
+                    </div>
+                    <div class="col d-flex justify-content-center">
+                        <ul class="list-unstyled">
+                            <li><h6 class="dropdown-header av-font-v fw-bold">Loans</h6></li>
+                            <li><a class="dropdown-item av-font-ii" href="#">Personal Loan</a></li>
+                            <li><a class="dropdown-item av-font-ii" href="#">Housing Loan</a></li>
+                            <li><a class="dropdown-item av-font-ii" href="#">Vehicle Loan</a></li>
+                            <li><a class="dropdown-item av-font-ii" href="#">Agri Loan</a></li>
+                            <li><a class="dropdown-item av-font-ii" href="#">Gold Loan</a></li>
+                        </ul>
+                    </div>
+                    <div class="col d-flex justify-content-center">
+                        <ul class="list-unstyled">
+                            <li><h6 class="dropdown-header av-font-v fw-bold">Insurance</h6></li>
+                            <li><a class="dropdown-item av-font-ii" href="#">APY</a></li>
+                            <li><a class="dropdown-item av-font-ii" href="#">PMJBY</a></li>
+                            <li><a class="dropdown-item av-font-ii" href="#">PMSBY</a></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
-            <div class="col-4"></div>
-            <div class="col-4"></div>
+        </div>
+        <div class="dropdown btn-group">
+            <button class="btn dropdown-toggle av-font-color-ii av-font-iv fw-bold" type="button" id="p-link-btn1" data-bs-toggle="dropdown" aria-expanded="false">
+            <i class="fas fa-seedling me-3"></i>PACS<i class="fas fa-chevron-down ms-3 btn-arrow"></i>
+            </button>
+            <div class="dropdown-menu p-4" aria-labelledby="p-link-btn1">
+                <div class="row">
+                    <div class="col d-flex justify-content-center">
+                        <ul class="list-unstyled">
+                            <li><h6 class="dropdown-header av-font-v fw-bold">Accounts & Deposits</h6></li>
+                            <li><a class="dropdown-item av-font-ii" href="#">Savings</a></li>
+                            <li><a class="dropdown-item av-font-ii" href="#">Current</a></li>
+                            <li><a class="dropdown-item av-font-ii" href="#">Recurring Deposit</a></li>
+                            <li><a class="dropdown-item av-font-ii" href="#">Term Deposit</a></li>
+                        </ul>
+                    </div>
+                    <div class="col d-flex justify-content-center">
+                        <ul class="list-unstyled">
+                            <li><h6 class="dropdown-header av-font-v fw-bold">Loans</h6></li>
+                            <li><a class="dropdown-item av-font-ii" href="#">Personal Loan</a></li>
+                            <li><a class="dropdown-item av-font-ii" href="#">Housing Loan</a></li>
+                            <li><a class="dropdown-item av-font-ii" href="#">Vehicle Loan</a></li>
+                            <li><a class="dropdown-item av-font-ii" href="#">Agri Loan</a></li>
+                            <li><a class="dropdown-item av-font-ii" href="#">Gold Loan</a></li>
+                        </ul>
+                    </div>
+                    <div class="col d-flex justify-content-center">
+                        <ul class="list-unstyled">
+                            <li><h6 class="dropdown-header av-font-v fw-bold">Insurance</h6></li>
+                            <li><a class="dropdown-item av-font-ii" href="#">APY</a></li>
+                            <li><a class="dropdown-item av-font-ii" href="#">PMJBY</a></li>
+                            <li><a class="dropdown-item av-font-ii" href="#">PMSBY</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="dropdown btn-group">
+            <button class="btn dropdown-toggle av-font-color-ii av-font-iv fw-bold" type="button" id="p-link-btn1" data-bs-toggle="dropdown" aria-expanded="false">
+            <i class="fas fa-briefcase me-3"></i>Business<i class="fas fa-chevron-down ms-3 btn-arrow"></i>
+            </button>
+            <div class="dropdown-menu p-4" aria-labelledby="p-link-btn1">
+                <div class="row">
+                    <div class="col d-flex justify-content-center">
+                        <ul class="list-unstyled">
+                            <li><h6 class="dropdown-header av-font-v fw-bold">Accounts & Deposits</h6></li>
+                            <li><a class="dropdown-item av-font-ii" href="#">Savings</a></li>
+                            <li><a class="dropdown-item av-font-ii" href="#">Current</a></li>
+                            <li><a class="dropdown-item av-font-ii" href="#">Recurring Deposit</a></li>
+                            <li><a class="dropdown-item av-font-ii" href="#">Term Deposit</a></li>
+                        </ul>
+                    </div>
+                    <div class="col d-flex justify-content-center">
+                        <ul class="list-unstyled">
+                            <li><h6 class="dropdown-header av-font-v fw-bold">Loans</h6></li>
+                            <li><a class="dropdown-item av-font-ii" href="#">Personal Loan</a></li>
+                            <li><a class="dropdown-item av-font-ii" href="#">Housing Loan</a></li>
+                            <li><a class="dropdown-item av-font-ii" href="#">Vehicle Loan</a></li>
+                            <li><a class="dropdown-item av-font-ii" href="#">Agri Loan</a></li>
+                            <li><a class="dropdown-item av-font-ii" href="#">Gold Loan</a></li>
+                        </ul>
+                    </div>
+                    <div class="col d-flex justify-content-center">
+                        <ul class="list-unstyled">
+                            <li><h6 class="dropdown-header av-font-v fw-bold">Insurance</h6></li>
+                            <li><a class="dropdown-item av-font-ii" href="#">APY</a></li>
+                            <li><a class="dropdown-item av-font-ii" href="#">PMJBY</a></li>
+                            <li><a class="dropdown-item av-font-ii" href="#">PMSBY</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
     <!-- Primary Links End-->
